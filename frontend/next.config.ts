d79924c30c03b8f4f416760bc833e0fd149eceaa
sojs-coder <sojscoder@gmail.com>
2026-04-19 11:29:57 -0700

    actual projections

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.csv$/i,
+      type: 'asset/source',
+    });
+
+    return config;
+  },
   async headers() {
     return [
       {
